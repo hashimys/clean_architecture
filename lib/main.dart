36ec3e7f048cc1f836b2d066566a34604bf57ff8
hashimys <hashimys1991@gmail.com>
2024-07-09 13:25:47 +0300

    fca 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:clean_architecture/core/app_theme.dart';
+import 'package:clean_architecture/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:clean_architecture/features/posts/presentation/bloc/posts/posts_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Posts'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (_) => di.sl<PostsBloc>()..add(GetAllPostsEvent())),
+        BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        title: 'Posts App',
+        home: Container(),
+        //PostsPage(),
       ),
     );
   }
